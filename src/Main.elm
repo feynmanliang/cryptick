5efc94096e152c5109b78bf2455835a02365a34b
Feynman Liang <feynman.liang@gmail.com>
2018-01-16 21:18:15 -0800

    Adds GDAX price fetch

--- src/Main.elm
+++ src/Main.elm
@@ -1,32 +1,95 @@
 module Main exposing (..)
 
-import Html exposing (Html, text, div, h1, img)
+import Debug
+import Html exposing (Html, text, div, h1, img, button)
 import Html.Attributes exposing (src)
+import Html.Events exposing (onClick)
+import Http
+import Json.Decode as Decode exposing (andThen)
 
 
 ---- MODEL ----
 
 
+type alias Price =
+    Float
+
+
 type alias Model =
-    {}
+    { bitcoinPrice : Maybe Price }
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( {}, Cmd.none )
+    ( { bitcoinPrice = Nothing
+      }
+    , getPrice BTC GDAX
+    )
+
+
+getPrice : Currency -> Exchange -> Cmd Msg
+getPrice currency exchange =
+    case exchange of
+        GDAX ->
+            case currency of
+                BTC ->
+                    Http.send NewPrice
+                        (Http.get "https://api.gdax.com/products/BTC-USD/stats"
+                            (decodeResponse currency exchange)
+                        )
+
+
+decodeResponse : Currency -> Exchange -> Decode.Decoder ( Currency, Exchange, Price )
+decodeResponse currency exchange =
+    let
+        stringToPrice x =
+            case String.toFloat x of
+                Ok n ->
+                    Decode.succeed ( currency, exchange, n )
+
+                Err _ ->
+                    Decode.fail "could not parse string into a floating point price"
+    in
+        (Decode.field "last" Decode.string)
+            |> andThen stringToPrice
 
 
 
 ---- UPDATE ----
 
 
+type Exchange
+    = GDAX
+
+
+type Currency
+    = BTC
+
+
 type Msg
     = NoOp
+    | NewPrice (Result Http.Error ( Currency, Exchange, Float ))
+    | RefreshPrice ( Currency, Exchange )
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    ( model, Cmd.none )
+    case msg of
+        NoOp ->
+            ( model, Cmd.none )
+
+        NewPrice (Ok ( currency, exchange, price )) ->
+            ( { model | bitcoinPrice = Just price }, Cmd.none )
+
+        NewPrice (Err err) ->
+            let
+                _ =
+                    Debug.log "err" err
+            in
+                ( model, Cmd.none )
+
+        RefreshPrice ( currency, exchange ) ->
+            ( model, getPrice currency exchange )
 
 
 
@@ -35,10 +98,21 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ img [ src "/logo.svg" ] []
-        , h1 [] [ text "Your Elm App is working!" ]
-        ]
+    let
+        bitcoinElem =
+            case model.bitcoinPrice of
+                Nothing ->
+                    "Loading"
+
+                Just p ->
+                    toString p
+    in
+        div []
+            [ img [ src "/logo.svg" ] []
+            , h1 [] [ text "GDAX Bitcoin Price" ]
+            , div [] [ text bitcoinElem ]
+            , button [ onClick (RefreshPrice ( BTC, GDAX )) ] [ text "Refresh" ]
+            ]
 
 
 
